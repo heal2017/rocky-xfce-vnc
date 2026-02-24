1321b9a89c19f5e4c8517d03f35edc3cdc4feb09
heal2017 <488411891@qq.com>
2026-02-24 09:06:53 +0800

    feat: show noVNC logs

--- server/resources/scripts/start_novnc.sh
+++ server/resources/scripts/start_novnc.sh
@@ -2,5 +2,12 @@
 set -Eeuo pipefail
 . ${RES_PATH}/scripts/define.sh
 
-${NOVNC_PATH}/utils/novnc_proxy --vnc localhost:5900 --listen ${NOVNC_PORT}
+if [ ${ENABLE_NOVNC} == "true" ]; then
+    ${NOVNC_PATH}/utils/novnc_proxy --vnc localhost:5900 --listen ${NOVNC_PORT} &> /dev/null &
+    if [ ${USE_VNC_PASSWORD} == "true" ]; then
+        information "no_vnc:    http://localhost:${NOVNC_PORT}/?password=${VNC_PASSWORD}"
+    else
+        information "no_vnc:    http://localhost:${NOVNC_PORT}"
+    fi
+fi
 
